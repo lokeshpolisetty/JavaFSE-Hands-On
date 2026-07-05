146a45aba3fa2ad7d375f30d5506bbb65d96c822
lokesh <polisettylokesh01@gmail.com>
2026-07-05 17:19:25 +0530

    3.ReactJS-HOL

---
+++ React/HOL-3/App.js
@@ -0,0 +1,17 @@
+import {CalculateScore} from './components/CalculateScore';
+
+function App()
+{
+
+return(
+<div>
+    <CalculateScore Name={"Polisetty Lokesh"}
+        School={"SRM University"}
+        total={50}
+        goal={0.6}
+    />
+</div>
+)
+}
+
+export default App;
